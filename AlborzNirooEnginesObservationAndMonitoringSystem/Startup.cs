0e9536f39344e5299f21cc0779ab28c8dbef9841
Mohammad Fakour Motlagh <mohammadfakour@gmail.com>
2019-01-10 15:08:00 +0330

    project startup

---
+++ AlborzNirooEnginesObservationAndMonitoringSystem/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AlborzNirooEnginesObservationAndMonitoringSystem.Startup))]
+namespace AlborzNirooEnginesObservationAndMonitoringSystem
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
